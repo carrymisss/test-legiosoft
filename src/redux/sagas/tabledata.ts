d30ec10dda105fe053ee55b9c2b42219b054bff8
carrymisss <56121366+carrymisss@users.noreply.github.com>
2021-08-17 20:13:22 +0300

    changed TS interface from IMockData to IDataItemsElement

--- src/redux/sagas/tabledata.ts
+++ src/redux/sagas/tabledata.ts
@@ -1,13 +1,7 @@
 import { takeEvery, put } from 'redux-saga/effects'
 import Actions from '../actions/tabledata'
+import { IDataItemsElement } from '../reducers/tabledata'
 
-export interface IMockData {
-    status: 'payment' | 'invoice' | 'withdrawal' | 'deposit',
-    type: 'payment' | 'invoice' | 'withdrawal' | 'deposit',
-    clientName: string,
-    amount: number,
-    transactionId: number
-}
 
 export default function* watchFetchTabledata(): any {
     yield takeEvery('FETCH_ITEMS', fetchTabledata)
@@ -15,7 +9,7 @@ export default function* watchFetchTabledata(): any {
 
 function* fetchTabledata() {
     yield put(Actions.setLoading(true))
-    const Tabledata: IMockData[] = yield fetch('https://6117dd4b30022f0017a06001.mockapi.io/mck')
+    const Tabledata: IDataItemsElement[] = yield fetch('https://6117dd4b30022f0017a06001.mockapi.io/mck')
     .then((response) => {
         return response.json()
     })
