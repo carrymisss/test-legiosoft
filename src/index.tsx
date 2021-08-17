2e72513b815af1b5116b3e542d5eaff5bcd835c0
carrymisss <56121366+carrymisss@users.noreply.github.com>
2021-08-17 20:06:21 +0300

    changed quotes in double to single in import from <@chakra-ui/react>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import reportWebVitals from './reportWebVitals'
