f7901c7d087624b4b2e866498a5423fbbcdd7516
carrymisss <56121366+carrymisss@users.noreply.github.com>
2021-08-20 01:28:02 +0300

    removed mockAPI action dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React, {useEffect} from 'react'
-import { useDispatch } from 'react-redux'
-import { Container, Flex, Spacer, Box, Stack, VStack, HStack } from '@chakra-ui/react'
-import Panel from './components/Panel'
-import Table from './components/Table'
-import Actions from './redux/actions/tabledata'
+import React from 'react'
+// import React, {useEffect} from 'react'
+// import { useDispatch } from 'react-redux'
+import { Container, VStack } from '@chakra-ui/react'
+import Panel from './containers/Panel'
+import Table from './containers/Table'
+// import Actions from './redux/actions/tabledata'
 
-// експорт по вибраних в дропадуні пунктах
 
 const App = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(Actions.fetchTabledata())
-  })
+  // useEffect(() => {
+  //   dispatch(Actions.fetchTabledata())
+  // })
 
   return (
-    <Container maxW="container.xl">
+    <Container maxW="container.lg">
       <VStack>
         <Panel />
         <Table />
