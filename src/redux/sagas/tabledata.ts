6e45e7fbca37ddabba0d6a806fcbe89409f00d2e
carrymisss <56121366+carrymisss@users.noreply.github.com>
2021-08-20 01:30:04 +0300

    Update tabledata.ts

--- src/redux/sagas/tabledata.ts
+++ src/redux/sagas/tabledata.ts
@@ -10,7 +10,7 @@ export default function* watchFetchTabledata(): any {
 function* fetchTabledata() {
     yield put(Actions.setLoading(true))
     const Tabledata: IDataItemsElement[] = yield fetch('https://6117dd4b30022f0017a06001.mockapi.io/mck')
-    .then((response) => {
+    .then((response) => {        
         return response.json()
     })
     yield put(Actions.setTabledata(Tabledata))
